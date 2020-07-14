e819b8769702e10a4363ad298dbc1f36dfbd4715
enes2101 <48836015+enes2101@users.noreply.github.com>
2020-07-14 20:44:03 +0300

    emreden-geldi

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float xRotation;
+    public float yRotation;
+    public float rotationSpeed;
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        player.transform.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/PlayerMovementController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    public float jumpForce;
+    public bool isGrounded = true;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Movement();
+        JumpInput();
+        CursorLockAndOpen();
+    }
+
+    public void JumpInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void Movement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.right * moveHorizontal + transform.forward * moveVertical;
+
+        transform.position += movement * Time.deltaTime * speed;
+        
+    }
+
+    private static void CursorLockAndOpen()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
